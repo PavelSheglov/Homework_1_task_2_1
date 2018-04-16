1ca3939570729abb9cf1027e83ac534e18ea9396
PavelSheglov <tpaul-1981@yandex.ru>
2018-04-16 22:42:13 +0700

    Задача по поиску значения в сгенерированном массиве

--- lec_1_hw_2_1/Program.cs
+++ lec_1_hw_2_1/Program.cs
@@ -10,6 +10,49 @@ namespace lec_1_hw_2_1
     {
         static void Main(string[] args)
         {
+            int N=0, A=0;
+            do
+            {
+                try
+                {
+                    Console.Write("Введите размерность массива N:");
+                    N = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine(N <= 0 ? "Размерность не может быть <= 0" : "");
+                }
+                catch(System.FormatException)
+                {
+                    Console.WriteLine("Не корректное число!!!");
+                }
+            } while (N<=0);
+            int[] arr = new int[N];
+            Random rnd = new Random();
+            const int leftborder = -100, rightborder = 100;
+            for (int i = 0; i < N; i++)
+                arr[i] = rnd.Next(leftborder, rightborder);
+            do
+            {
+                do
+                {
+                    try
+                    {
+                        Console.Write($"Введите искомое целое число A в диапазоне [{leftborder}, {rightborder}]:");
+                        A = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine((A < leftborder || A > rightborder) ? "Число не попадает в диапазон" : "");
+                    }
+                    catch (System.FormatException)
+                    {
+                        Console.WriteLine("Не корректное число!!!");
+                    }
+                } while (A < leftborder || A > rightborder);
+                Console.WriteLine("Есть ли число A в массиве?");
+                Console.WriteLine(Array.IndexOf(arr, A) >= 0 ? "Yes" : "No");
+                Console.WriteLine();
+                Console.WriteLine("Продолжить: Да (1), Нет (any)?");
+            } while (Console.ReadLine()=="1");
+            Console.WriteLine("Содержимое массива");
+            foreach (int i in arr)
+                Console.Write(i + " ");
+            Console.WriteLine();
         }
     }
 }
