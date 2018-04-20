0abca26fd17fda3c655891b6fadaed6b8bcbfe13
PavelSheglov <tpaul-1981@yandex.ru>
2018-04-20 14:28:30 +0700

    Разбивка на функции

--- lec_1_hw_2_1/Program.cs
+++ lec_1_hw_2_1/Program.cs
@@ -8,9 +8,24 @@ namespace lec_1_hw_2_1
 {
     class Program
     {
+        const int leftBorder = -100, rightBorder = 100;
+
         static void Main(string[] args)
         {
-            int N=0, A=0;
+            int N=InputArraySize(), A=0;
+            int[] arr = GenarateArray(N, leftBorder, rightBorder);
+            do
+            {
+                A = InputNumber();
+                ShowResultOfSearchingNumberInArray(arr, A);
+                Console.WriteLine("Продолжить: Да (1), Нет (any)?");
+            } while (Console.ReadLine()=="1");
+            ShowArray(arr);
+        }
+
+        static int InputArraySize()
+        {
+            int N = 0;
             do
             {
                 try
@@ -19,38 +34,53 @@ namespace lec_1_hw_2_1
                     N = Convert.ToInt32(Console.ReadLine());
                     Console.WriteLine(N <= 0 ? "Размерность не может быть <= 0" : "");
                 }
-                catch(System.FormatException)
+                catch (System.FormatException)
                 {
                     Console.WriteLine("Не корректное число!!!");
                 }
-            } while (N<=0);
-            int[] arr = new int[N];
+            } while (N <= 0);
+            return N;
+        }
+
+        static int[] GenarateArray(int size, int leftBorder, int rightBorder)
+        {
+            int[] arr = new int[size];
             Random rnd = new Random();
-            const int leftborder = -100, rightborder = 100;
-            for (int i = 0; i < N; i++)
-                arr[i] = rnd.Next(leftborder, rightborder);
+            for (int i = 0; i < size; i++)
+                arr[i] = rnd.Next(leftBorder, rightBorder);
+            return arr;
+        }
+
+        static int InputNumber()
+        {
+            int number = 0;
             do
             {
-                do
+                try
                 {
-                    try
-                    {
-                        Console.Write($"Введите искомое целое число A в диапазоне [{leftborder}, {rightborder}]:");
-                        A = Convert.ToInt32(Console.ReadLine());
-                        Console.WriteLine((A < leftborder || A > rightborder) ? "Число не попадает в диапазон" : "");
-                    }
-                    catch (System.FormatException)
-                    {
-                        Console.WriteLine("Не корректное число!!!");
-                    }
-                } while (A < leftborder || A > rightborder);
-                Console.WriteLine("Есть ли число A в массиве?");
-                Console.WriteLine(Array.IndexOf(arr, A) >= 0 ? "Yes" : "No");
-                Console.WriteLine();
-                Console.WriteLine("Продолжить: Да (1), Нет (any)?");
-            } while (Console.ReadLine()=="1");
+                    Console.Write($"Введите искомое целое число A в диапазоне [{leftBorder}, {rightBorder}]:");
+                    number = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine((number < leftBorder || number > rightBorder) ? "Число не попадает в диапазон" : "");
+                }
+                catch (System.FormatException)
+                {
+                    Console.WriteLine("Не корректное число!!!");
+                }
+            } while (number < leftBorder || number > rightBorder);
+            return number;
+        }
+
+        static void ShowResultOfSearchingNumberInArray(int[] array, int number)
+        {
+            Console.WriteLine("Есть ли число A в массиве?");
+            Console.WriteLine(Array.IndexOf(array, number) >= 0 ? "Yes" : "No");
+            Console.WriteLine();
+        }
+
+        static void ShowArray(int[] array)
+        {
             Console.WriteLine("Содержимое массива");
-            foreach (int i in arr)
+            foreach (int i in array)
                 Console.Write(i + " ");
             Console.WriteLine();
         }
